a1588dbef78ace7cfed704dc7b3be3b08c1650da
evaits <musicevait@gmail.com>
2023-02-12 23:10:40 +0100

    add script

--- script/home.js
+++ script/home.js
@@ -10,6 +10,8 @@ for(let i = 0; i<arr.length; i++){
   arr[i] = Number(arr[i])
 }
 
+
+
 // Print today statistic
 let push = document.querySelector('.repeat')
 let pod = document.querySelector('.approach')
@@ -23,4 +25,42 @@ arr[0] == 0 ? pod.innerHTML = 0 : pod.innerHTML = arr.length
 let average = document.querySelector('.average')
 average.innerHTML = Math.round(sum/arr.length) + '/ap'
 
+
+
 // Daily goal
+let daily_percent = sum/100*100+'%'
+if(sum<=100){
+  document.querySelector('.percent').innerHTML = daily_percent
+  document.querySelector('.done').style.width = daily_percent
+}
+else {
+  document.querySelector('.percent').innerHTML = daily_percent
+  document.querySelector('.done').style.width = '100%'
+}
+
+
+
+        // Sort and get Dates
+let ms = []
+for(let i = 0; i<localStorage.length; i++){
+  if((localStorage.key(i)=='arr') || (localStorage.key(i) == "today")){
+      continue
+  }
+
+  ms.push(Date.parse(localStorage.key(i)))
+  
+}
+
+ms.sort((a, b) => b - a);
+
+const date = []
+for(let i = 0; i<ms.length; i++){
+  date.push(Intl.DateTimeFormat('en-GB').format(ms[i]))
+}
+console.log(date)
+
+
+        // Print Activity Progress
+if(localStorage.length-2 > 0){
+  let blocks = document.querySelector('.activity-blocks')
+}
--- script/modal.js
+++ script/modal.js
@@ -1,4 +1,4 @@
-// Modal
+      // Modal
 let count_modal = 0
 function open_modal() {
     if(count_modal%2==0){
@@ -13,7 +13,7 @@ function open_modal() {
     }
 }
 
-// Animation
+      // Animation
 function animateOpenModal() {
     let plus = document.querySelector('.plus')
     var animation = plus.animate([
@@ -36,19 +36,72 @@ function animateCloseModal() {
       });
 }
 
-// Save data
+      // Save data
 function item(digit) {
   let item_id = digit
   window.item_global = item_id;
 }
 
 function save_data() {
+  // Get global var
   let item_id = window.item_global;
   if(item_id==undefined) {
     item_id = 1
   }
-  console.log(item_id)
+
+
+  // Work with arr and Local Storage
+  let arr = []
+  arr = localStorage.getItem("arr")
+  arr = arr.split(',')
+  for(let i = 0; i<arr.length; i++){
+      arr[i] = Number(arr[i])
+  }
+
+  if(arr[0] == 0){
+    arr.shift()
+  }
+  arr.push(item_id)
+  localStorage.setItem("arr", arr)
+
+  // Close modal and reload
   animateCloseModal()
   document.querySelector('.modal-bg').style.display = 'none'
-  location=location
+  location.href=location.href;
 }
+
+
+// Save Date
+let today = new Date()
+today = Intl.DateTimeFormat('en-GB').format(today)
+today = today.slice(0, 10)
+if(localStorage.getItem("today") == null){
+  localStorage.setItem("today", today)
+}
+
+let sum1 = 0
+if (localStorage.getItem('arr')){ 
+  
+  let arr1 = []
+  arr1 = localStorage.getItem("arr")
+  arr1 = arr1.split(',')
+  for(let i = 0; i<arr1.length; i++){
+    arr1[i] = Number(arr1[i])
+  }
+  arr1.map((item) => sum1 += item);
+}
+
+
+
+let localStorage_date = localStorage.getItem('today')
+if(localStorage_date !== today){
+  localStorage.setItem(localStorage_date, sum1)
+  localStorage.setItem("today", today)
+  
+  localStorage.removeItem("arr")
+  location = location
+}
+
+
+
+
