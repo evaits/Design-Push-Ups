8404c9a0ce6b61bd28091f57f2f14287934cf7b0
evaits <musicevait@gmail.com>
2023-02-21 01:38:41 +0100

    Add style for Invalid Data

--- start/script/start2.js
+++ start/script/start2.js
@@ -62,60 +62,118 @@ $(document).ready(function () {
 		}
         }
 	});
+  
 }); 
 
 // Get data
 function save_data() {
   // First Name
-  let firstName = document.querySelector('#FirstName').value
-  if(firstName.length < 2){
-    alert('Error')
-    return
+  let count = 0
+  let firstName = document.querySelector('#FirstName')
+  let block_firstName = document.querySelector('.firstName')
+  if(firstName.value.length < 2){
+    firstName.classList.add('placeholder')
+    block_firstName.style.background = '#D7D7D7'
+    firstName.style.background = '#D7D7D7'
+    block_firstName.onclick =  function(){
+      firstName.classList.remove('placeholder')
+      block_firstName.style.background = '#F7F8F8'
+      firstName.style.background = '#F7F8F8'
+    }
+    count++
   }
 
   // Last Name
-  let lastName = document.querySelector('#LastName').value
-  if(lastName.length < 2){
-    alert('Error')
-    return
+  let lastName = document.querySelector('#LastName')
+  let block_lastName = document.querySelector('.lastName')
+  if(lastName.value.length < 2){
+    lastName.classList.add('placeholder')
+    block_lastName.style.background = '#D7D7D7'
+    lastName.style.background = '#D7D7D7'
+    block_lastName.onclick =  function(){
+      lastName.classList.remove('placeholder')
+      block_lastName.style.background = '#F7F8F8'
+      lastName.style.background = '#F7F8F8'
+    }
+    count++
   }
 
   // Gender
-  let gender = window.gender_global;
-  if(gender == undefined ){alert('Error')}
+  let gender_value = window.gender_global;
+  let gender = document.querySelector('#gender')
+  let context_gender = document.querySelector('.context')
+  let block_gender = document.querySelector('.gender')
+  if(gender_value == undefined){
+    gender.classList.add('placeholder')
+    context_gender.style.background = '#D7D7D7'
+    gender.style.background = '#D7D7D7'
+    block_gender.onclick =  function(){
+      gender.classList.remove('placeholder')
+      context_gender.style.background = '#F7F8F8'
+      gender.style.background = '#F7F8F8'
+    }
+    count++
+  }
 
   // Age
-  age = document.querySelector('.age').value
-  if((age < 3) || (age > 100)){
-    alert('Error')
-    return
+  age = document.querySelector('.age')
+  let block_age = document.querySelector('.birthday')
+  if((age.value < 3) || (age.value > 100)){
+    age.classList.add('placeholder')
+    block_age.style.background = '#D7D7D7'
+    age.style.background = '#D7D7D7'
+    block_age.onclick =  function(){
+      age.classList.remove('placeholder')
+      block_age.style.background = '#F7F8F8'
+      age.style.background = '#F7F8F8'
+    }
+    count++
   }
 
   // Weight
-  weight = document.querySelector('.weight').value
-  if((weight < 10) || (weight > 300)){
-    alert('Error')
-    return
+  weight = document.querySelector('.weight_inp')
+  let block_weight = document.querySelector('.weight')
+  if((weight.value < 10) || (weight.value > 300)){
+    weight.classList.add('placeholder')
+    block_weight.style.background = '#D7D7D7'
+    weight.style.background = '#D7D7D7'
+    block_weight.onclick =  function(){
+      weight.classList.remove('placeholder')
+      weight.style.background = '#F7F8F8'
+      block_weight.style.background = '#F7F8F8'
+    }
+    count++
   }
 
   // Height
-  height = document.querySelector('.height').value
-  if((height < 50) || (height > 250)){
-    alert('Error')
-    return
+  height = document.querySelector('.height_inp')
+  let block_height = document.querySelector('.height')
+  if((height.value < 50) || (height.value > 250)){
+    height.classList.add('placeholder')
+    block_height.style.background = '#D7D7D7'
+    height.style.background = '#D7D7D7'
+    block_height.onclick =  function(){
+      height.classList.remove('placeholder')
+      block_height.style.background = '#F7F8F8'
+      height.style.background = '#F7F8F8'
+    }
+    count++
   }
 
+  if(count != 0){alert('Invalid Data'); return}
+
   user = {
-    FirstName: firstName,
-    LastName: lastName,
-    sex: gender,
-    age: age,
-    weight: weight,
-    height: height
+    FirstName: firstName.value,
+    LastName: lastName.value,
+    sex: gender_value.value,
+    age: age.value,
+    weight: weight.value,
+    height: height.value
   }
   localStorage.setItem('user', JSON.stringify(user));
   location = 'start3.html'
 }
+
 
 
 
